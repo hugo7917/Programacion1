4aad4d3e6a674172f216317b16010281445d601f
hugo7917 <hugo.carballas@campus.xaloc.org>
2024-09-28 15:38:18 +0200

    Update ejercicio 10.py

--- Python/Ejercicios_iniciales/ejercicio 10.py
+++ Python/Ejercicios_iniciales/ejercicio 10.py
@@ -1,5 +1,5 @@
 
-
+#10. Introduce por teclado dos números y muestre por pantalla la siguiente información: cociente, resto y si el dividendo es par o impar. 
 var1=float(input("dime un numer: "))
 var2=float(input("dime otro numero: "))
 print("El coeficiente es:", round(var1/var2, 2))
