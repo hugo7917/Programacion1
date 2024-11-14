625c32f545a3b09fb16a32566271c5e285b1c0ec
hugo7917 <hugo.carballas@campus.xaloc.org>
2024-11-14 09:31:30 +0100

    Update PASSWORD.py

--- Python/PASSWORD.py
+++ Python/PASSWORD.py
@@ -1,12 +1,62 @@
+#Realitzar un programa que permeti introduir una ‘paraula clau’ amb unes carcteristiques.
 
-#contador
-var="abcdefg"
+print("Estas son las reglas de el password:")
+print("posición 1. Un numero mayor o igual a 1 y menor o igual que 5. ")
+print("posición 2. Una letra minuscula. ")
+print("posición 3. Una letra mayuscula. ")
+print("posición 4. Uno de los siguientes simbolos: *, _, @. ")
+print("posición 5. Una letra minuscula. ")
+print("posición 6. Un numero mayor o igual a 6 y menor o igual que 9. ")
+print("posición 7. Uno de los siguientes simbolos: &, /, #. ")
+print("posición 8. Un numero menor o igual que 5. ")
 
-#for contador in range (10):
+password=input("introduce una contraseña de entre 6 y 8 caracteres:")
+        
+if len(password)==8 or len(password)==7 and len(password)==6:
+    if password[0]<str(1) or password[0]>str(5) :
+        print("El error esta en el caracter 1")
+    
+    if password[1].islower()== False:
+        print("El error esta en el carcter 2")
 
-for contador in var:
-    print("el valor uno es ",cont,"es,contador")
-    cont=cont+1
+    if password[2].isupper()== False:
+        print("El error esta en el carcter 3")
+
+    if password[3]=="*"==False:
+        print("El error esta en el carcter 4")
+        if password[3]=="_"==False: 
+            print("El error esta en el carcter 4")
+            if password[3]=="@"== False:
+                print("El error esta en el carcter 4")
+
+    if password[4].islower()== False:
+        print("El error esta en el carcter 5")
+
+    if password[5]<str(6) or password[5]>str(9) :
+        print("El error esta en el caracter 6")
+
+    if len(password)<6 or len(password)>8:
+        if password[6]=="&"== False: 
+            print("El error esta en el carcter 7")            
+            if password[3]=="/"== False: 
+                print("El error esta en el carcter 7")                
+                if password=="#"== False:
+                    print("El error esta en el carcter 7")
+        
+        if len(password)==8:
+            if password[7]>str(5) or password[7]==(5) :
+                print("El error esta en el caracter 8")
+
+if len(password)<6: 
+    print("La contraseña no llega al numero de caracteres necesarios")
+    
+if len(password)>8:
+    print("La contraseña supera el numero de carcteres permitidos")
+    
+    
+else:
+    print("La contraseña cumple los requsitos")
+
 
 
 
