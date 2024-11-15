1991d7cc94f7ee61b11e533751cd142599cfd9ed
hugo7917 <hugo.carballas@campus.xaloc.org>
2024-11-15 10:46:33 +0100

    Update PASSWORD.py

--- Python/PASSWORD.py
+++ Python/PASSWORD.py
@@ -12,7 +12,7 @@ print("posición 8. Un numero menor o igual que 5. ")
 
 password=input("introduce una contraseña de entre 6 y 8 caracteres:")
         
-if len(password)==8 or len(password)==7 and len(password)==6:
+if len(password)==8 or len(password)==7 or len(password)==6:
     if password[0]<str(1) or password[0]>str(5) :
         print("El error esta en el caracter 1")
     
@@ -22,30 +22,22 @@ if len(password)==8 or len(password)==7 and len(password)==6:
     if password[2].isupper()== False:
         print("El error esta en el carcter 3")
 
-    if password[3]=="*"==False:
+    if password[3] not in "*_@":
         print("El error esta en el carcter 4")
-        if password[3]=="_"==False: 
-            print("El error esta en el carcter 4")
-            if password[3]=="@"== False:
-                print("El error esta en el carcter 4")
-
+        
     if password[4].islower()== False:
         print("El error esta en el carcter 5")
 
     if password[5]<str(6) or password[5]>str(9) :
         print("El error esta en el caracter 6")
 
-    if len(password)<6 or len(password)>8:
-        if password[6]=="&"== False: 
+    elif len(password)==7 or len(password)==8:
+        if password[6] not in ["&","/","#"]:
             print("El error esta en el carcter 7")            
-            if password[3]=="/"== False: 
-                print("El error esta en el carcter 7")                
-                if password=="#"== False:
-                    print("El error esta en el carcter 7")
-        
-        if len(password)==8:
-            if password[7]>str(5) or password[7]==(5) :
-                print("El error esta en el caracter 8")
+
+    elif len(password)==8:
+        if password[7]>str(5) or password[7]==(5) :
+            print("El error esta en el caracter 8")
 
 if len(password)<6: 
     print("La contraseña no llega al numero de caracteres necesarios")
@@ -54,9 +46,7 @@ if len(password)>8:
     print("La contraseña supera el numero de carcteres permitidos")
     
     
-else:
-    print("La contraseña cumple los requsitos")
-
+    #print("La contraseña cumple los requsitos")
 
 
 
