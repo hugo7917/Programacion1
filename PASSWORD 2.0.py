5b01894353d592b46bc30cc1877d2faa05d65865
hugo7917 <hugo.carballas@campus.xaloc.org>
2024-12-12 09:13:06 +0100

    Add files via upload

--- PASSWORD 2.0.py
+++ PASSWORD 2.0.py
@@ -1,11 +1,16 @@
 #Realitzar un programa que permeti introduir una ‘paraula clau’ amb unes carcteristiques.
 
-print("INSTRUCCIONES: La longitud del password a de ser de 8 carcteres, debe de tener 1 letra mayuscula y 2 minusculas, 2 numeros entre 1-5 y 1 entre 6-9, y por ultimo debe de tenr 2 simbolos. ")
+print("INSTRUCCIONES: La longitud del password a de ser de 8 carcteres, debe de tener 1 letra mayuscula y 2 minusculas, 3 numeros entre 1-5 y 2 simbolos de estos: /, *, #, @")
 
 
 password=input("Introduce la contraseña: ")
-
-if len
+var_mayusculas="ABCDEFGHIJKLMNÑOPKRSTUVWXYZÁÉÍÓÚ"
+var_minuscvulas="abcdefghijklmnñopqrstuvwxyzáéíóú"
+var_num="12345"
+var_simbolos= "/*#@"
+if len(password)= 8:
+    if var_mayusculas= 1 or var_minusculas= 2 or var_num= :
+        
     
 
 
