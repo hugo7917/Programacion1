1ca1ec1afff6ae3c7b721e0448354929eaaa7c2a
hugo7917 <hugo.carballas@campus.xaloc.org>
2025-01-27 19:21:38 +0100

    Update ejercicio 51.py

--- Python/While/ejercicio 51.py
+++ Python/While/ejercicio 51.py
@@ -8,26 +8,3 @@ while num_buenosdias<buenos_dias:
     num_buenosdias+=1
 
 
-
-
-
-
-
-
-=======
-#51. A partir del programa anterior, modifica el código para que sea el usuario quién introduzca el número de veces que desea que repita la frase Buenos días. Con While
-
-buenos_dias=int(input("Introduce el numero de veces que quiera que rebita buenos dias: "))
-num_buenosdias=0
-while num_buenosdias<buenos_dias:
-    print("buenos dias")
-    num_buenosdias+=1
-
-
-
-
-
-
-
-
->>>>>>> b1e0f5e5dbf9d2ff07982fe395969f12be400cf0
