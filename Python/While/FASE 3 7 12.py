82e697f068d22d221420ebcc941e50ac74f61c17
hugo7917 <hugo.carballas@campus.xaloc.org>
2025-03-07 10:09:46 +0100

    Update FASE 3 7 12.py

--- Python/While/FASE 3 7 12.py
+++ Python/While/FASE 3 7 12.py
@@ -7,11 +7,11 @@ tipos_cartas = random.randint(1,7)
 valores_cartas = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7,10: 0.5, 11: 0.5, 12: 0.5}
 
 def mostrar_puntuacion(puntuacion):
-    print(f"Puntuación actual: {puntuacion:.1f}")
+    print(f" {"Alias"} tu puntuación actual es de: {puntuacion:.1f}")
 
 def jugar():
     deposito = 100  
-    print(f"Tienes un depósito inicial de {deposito} puntos.")
+    print(f"{"Alias"} tienes un depósito inicial de {deposito} puntos.")
     
     while deposito > 0:
         print(f"\nTu depósito tiene {deposito} puntos.")
@@ -31,26 +31,26 @@ def jugar():
 #Esta parte del program he decidido cambiarla un poco más para que el programa no sea tan seco y no parezca que te está 
 #hablando una maquina y así, de paso, incito al jugador a que vuelva a jugar con algunas frases.
             if puntuacion_jugador < 7.5:
-                continuar = input("¿Quieres otra carta? (si/no): ")
+                continuar = input("¿Quieres otra carta {"Alias"}? (si/no): ")
         
         if puntuacion_jugador == 7.5:
-            print("\n¡Enhorabuena, has ganado la partida! ¡Estas en racha, vuelve a probar y quizá vuelvas a ganar!")
+            print("\n¡Enhorabuena {"Alias"}, has ganado la partida! ¡Estas en racha, vuelve a probar y quizá vuelvas a ganar!")
             deposito += 10
         elif puntuacion_jugador > 7.5:
-            print("\n¡Has perdido la partida! Te has pasado de 7.5.¡Vuelve a proba y quizá esta vez ganes!")
+            print("\n¡Has perdido la partida {"Alias"}! Te has pasado de 7.5.¡Vuelve a proba y quizá esta vez ganes!")
             deposito -= 10
         elif puntuacion_jugador >= 6:
-            print("\nTe has plantado. ¡Has sabido plantarte a tiempo!.")
+            print("\nTe has plantad {"Alias"}o. ¡Has sabido plantarte a tiempo!.")
             deposito += 5
         else:
-            print("\nTe has plantado. Quizás tendrías que arriesgar un poco ¿no?, ya que ¡Quien no arriesga no gana!")
+            print("\nTe has plantado {"Alias"}. Quizás tendrías que arriesgar un poco ¿no?, ya que ¡Quien no arriesga no gana!")
             deposito -= 5
         
         if deposito <= 0:
-            print("\nTu depósito se ha agotado. Ya no puedes continuar jugando, ¡ahorra y vuelve cuando puedas!.")
+            print("\nTu depósito se ha agotado {"Alias"}. Ya no puedes continuar jugando, ¡ahorra y vuelve cuando puedas!.")
             break
         
-        jugar_otra_vez = input("\n¿Quieres jugar otra vez? (si/no): ")
+        jugar_otra_vez = input("\n¿Quieres jugar otra vez {"Alias"}? (si/no): ")
         if jugar_otra_vez.lower() != "si":
             break
 
